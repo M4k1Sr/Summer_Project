3d9a50860869f0219019b40b958cfda693f95bf6
DESKTOP-GHBF67C\Owner <yanagiibuki3400@gmail.com>
2026-08-05 17:18:52 +0900

    タイトルロゴ追加

--- Project/Src/Scene/Title/TitleScene.cpp
+++ Project/Src/Scene/Title/TitleScene.cpp
@@ -6,11 +6,67 @@
 #include "../../Manager/Font/FontManager.h"
 #include "../../Manager/Input/KeyManager.h"
 #include "../SceneManager/SceneManager.h"
+#include "../../Application/Application.h"
 
 #include "../Test/TestScene.h"
 
 TitleScene::TitleScene(void) : SceneBase()
 {
+	titleLogoHandle_ = -1;
+	state_ = TitleState::Title;
+}
+
+void TitleScene::SubPreInit(void)
+{
+	pos_ = Vector2(App::SCREEN_SIZE_X_HALF, App::SCREEN_SIZE_Y_HALF);
+
+	animTime_ = float(TitleState::Title);
+
+}
+
+void TitleScene::SubPreLoad(void)
+{
+	titleLogoHandle_ = LoadGraph("Data/Image/Title/TitleLogo.png");
+}
+
+void TitleScene::SubPreUpdate(void)
+{
+	switch (state_)
+	{
+	case TitleState::Title:
+		if (animTime_ <= 0.0f)
+		{
+			state_ = TitleState::Movie;
+		}
+		break;
+	case TitleState::Movie:
+	{
+		//タイトルロゴの座標を右下に移動させる
+		int targetX = App::SCREEN_SIZE_X - TITLE_LOGO_WIDTH_HALF;
+		int targetY = App::SCREEN_SIZE_Y - TITLE_LOGO_HEIGHT_HALF;
+		float dx = targetX - pos_.x;
+		float dy = targetY - pos_.y;
+
+		float length = std::sqrt(dx * dx + dy * dy);
+
+		if (length <= 2.0f)
+		{
+			pos_.x = targetX;
+			pos_.y = targetY;
+		}
+		else
+		{
+			pos_.x += dx / length*2.0f;
+			pos_.y += dy / length*2.0f;
+		}
+
+		break;
+	}
+	case TitleState::MoveToDemo:
+		break;
+	default:
+		break;
+	}
 }
 
 void TitleScene::SubPostUpdate(void)
@@ -32,10 +88,19 @@ void TitleScene::SubPostUpdate(void)
 	if (Key::GetIns().GetInfo(KEY_TYPE::DEBUG_RELOAD).down) {
 		SceneManager::GetIns().ChangeSceneFade(std::make_unique<TestScene>());
 	}
+
+	// タイトルロゴのアニメーション
+	animTime_ -= 1.0f/60.0f;	//仮のやつ
+
+
 }
 
 void TitleScene::SubPostUiDraw(void)
 {
 	DrawStringToHandle(0, 0, "タイトル", 0xffffff, Font::GetIns().GetFont(FontKinds::DEFAULT_45));
 	DrawStringToHandle(0, 45, "「-」キーでテストシーンに遷移", 0xffffff, Font::GetIns().GetFont(FontKinds::DEFAULT_20));
+
+	//タイトルロゴ
+	DrawRotaGraph(pos_.x, pos_.y,1.0f,0.0f, titleLogoHandle_, TRUE);
+
 }
--- Project/Src/Scene/Title/TitleScene.h
+++ Project/Src/Scene/Title/TitleScene.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "../SceneBase.h"
+#include "../../Common/Vector2.h"
 
 class TitleScene : public SceneBase
 {
@@ -9,6 +10,31 @@ public:
 	~TitleScene(void)override = default;
 
 private:
+	void SubPreInit(void)override;
+	void SubPreLoad(void)override;
+	void SubPreUpdate(void)override;
 	void SubPostUpdate(void)override;
 	void SubPostUiDraw(void)override;
+
+private:
+
+	//タイトルシーンの状態遷移
+	enum class TitleState
+	{
+		Title = 5,		//タイトル画面
+		Movie = 10,		//ロゴの後ろでゲームムービーを流す
+		MoveToDemo,	//デモシーンへ遷移
+	};
+
+	static constexpr int TITLE_LOGO_WIDTH = 552;	//タイトルロゴの横幅
+	static constexpr int TITLE_LOGO_HEIGHT = 106;	//タイトルロゴの縦幅
+	static constexpr int TITLE_LOGO_WIDTH_HALF = 276;	//タイトルロゴの横幅の半分
+	static constexpr int TITLE_LOGO_HEIGHT_HALF = 53;	//タイトルロゴの縦幅の半分
+
+	//メンバ変数
+	int titleLogoHandle_;		//タイトルロゴのハンドル
+	TitleState state_;		//タイトルシーンの状態
+	Vector2 pos_;		//タイトルロゴの座標
+	float animTime_;	//タイトルロゴのアニメーション時間
+
 };
