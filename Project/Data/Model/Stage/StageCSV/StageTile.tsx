abd824e7c11ecddc21bff5165a23da666bba7565
ASOST\2416014 <shimurushimuru81@gmail.com>
2026-09-01 14:49:08 +0900

    マップチップ追加

---
+++ Project/Data/Model/Stage/StageCSV/StageTile.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="StageTile" tilewidth="64" tileheight="64" tilecount="130" columns="13">
+ <image source="../Downloads/mapchip-sizeup001.png" width="846" height="650"/>
+</tileset>
--- Project/Src/Object/Stage/DemoStage/DemoStage.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-class DemoStage
-	: public ActorBase
-{
-};
-
